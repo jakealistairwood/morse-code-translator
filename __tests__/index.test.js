c8498a71fb9106195de257a1e344f2ee5e5152e7
Jake Wood <Jake.Wood@nology.io>
2022-02-26 15:34:28 +0000

    added styles

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,24 +2,32 @@ import { translateEnglishToMorse } from "../translator.js";
 import { expect, it, describe } from "@jest/globals";
 
 describe("translate words from english to morse", () => {
+    it("should check input is of type: string", () => {
+        const result = translateEnglishToMorse("hello");
+        expect(typeof result).toBe("string");
+    });
+    it("should accept special characters in the input", () => {
+        const result = translateEnglishToMorse("Hello world! My Name is Jake");
+        expect(result).toBe(
+            ".... . .-.. .-.. --- .-- --- .-. .-.. -.. ..--. -- -.-- -. .- -- . .. ... .--- .- -.- ."
+        );
+    });
     // positive test case for a single word
     it("should translate a word correctly to morse code", () => {
         const result = translateEnglishToMorse("hello");
         expect(result).toBe(".... . .-.. .-.. ---");
     });
     it("should remove any spaces in the input before converting", () => {
-        const result = translateEnglishToMorse("      ");
-        expect(result).toBe("");
+        const result = translateEnglishToMorse("      hello");
+        expect(result).toBe(".... . .-.. .-.. ---");
     });
     it("should clean a word by converting input to lowercase", () => {
         const result = translateEnglishToMorse("hELlo");
         expect(result).toBe(".... . .-.. .-.. ---");
     });
     it("should accept and translate numbers", () => {
-        const result = translateEnglishToMorse("0123456789");
-        expect(result).toBe(
-            "----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----."
-        );
+        const result = translateEnglishToMorse("h3ll0");
+        expect(result).toBe(".... ...-- .-.. .-.. -----");
     });
     // positive tests for a single letter
     it("should check single letter returns the correct morse", () => {
@@ -27,8 +35,10 @@ describe("translate words from english to morse", () => {
         expect(result).toBe(".-");
     });
     // positive tests for multiple words
-
-    // negative tests for edge cases
+    it("should translate a sentence correctly to morse", () => {
+        const result = translateEnglishToMorse("Hello world my name is Jake");
+        expect(result).toBe(
+            ".... . .-.. .-.. --- .-- --- .-. .-.. -.. -- -.-- -. .- -- . .. ... .--- .- -.- ."
+        );
+    });
 });
-
-// describe("translates morse code to english", () => {});
