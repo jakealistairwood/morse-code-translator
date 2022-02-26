30b2679658aadf573ee88014696b76b789b68eb3
Jake Wood <Jake.Wood@nology.io>
2022-02-26 19:56:07 +0000

    majority of styles in situ & added clear btn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import { translateEnglishToMorse } from "./translator.js";
 
 const translatorInput = document.querySelector(".translator__input");
-const translatorBtn = document.querySelector(".translator__btn");
+const translatorBtn = document.querySelector(".translator__btn--translate");
+const translatorClearBtn = document.querySelector(".translator__btn--clear");
 const translatorOutput = document.querySelector(".translator__output");
+
+const clearOutput = () => {
+    return (translatorOutput.innerText = "");
+};
 
 translatorBtn.addEventListener("click", () => {
     translatorOutput.innerText = translateEnglishToMorse(translatorInput.value);
 });
+
+translatorClearBtn.addEventListener("click", clearOutput);
